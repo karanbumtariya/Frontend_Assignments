bea1cf71a1c449897018f90711a076c3ac152135
Karan Bumtariya <karanbumtariya05@gmail.com>
2026-01-20 12:13:13 +0530

    Add files via upload

---
+++ Graphql_Next.js/my-app/contact/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ const contact = () => {
+  return (
+    <div>
+        <h3>Contact page</h3>
+    </div>
+  )
+}
+export default contact
---
+++ Graphql_Next.js/my-app/menu.tsx
@@ -0,0 +1,9 @@
+export default function Menu() {
+  return (
+    <div>
+      <a href="/">Home || </a>
+      <a href="/about">About || </a>
+      <a href="/contact">Contact</a>
+    </div>
+  );
+}
